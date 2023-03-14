d9c7c708f91ede80fc666e871c3d91cf9fc5adc1
Jason Yang <jasonxuyang@gmail.com>
2023-03-14 10:43:58 -0400

    feature: add events

--- data/events.ts
+++ data/events.ts
@@ -2,6 +2,7 @@ export enum TIME {
   MORNING = "morning",
   AFTERNOON = "afternoon",
   NIGHT = "night",
+  ALL_DAY = "all day",
 }
 
 export enum PILLAR {
@@ -20,8 +21,6 @@ export type EVENT = {
   time: TIME;
   effects: EFFECT[];
 };
-
-
 
 export const EVENTS: EVENT[] = [
   {
@@ -47,6 +46,64 @@ export const EVENTS: EVENT[] = [
       { pillar: PILLAR.SLEEP, value: -5 },
       { pillar: PILLAR.STUDY, value: -5 },
       { pillar: PILLAR.SOCIAL, value: 10 },
+    ],
+  },
+  {
+    title: "Attend career fair?",
+    time: TIME.AFTERNOON,
+    effects: [
+      { pillar: PILLAR.STUDY, value: 10 },
+      { pillar: PILLAR.SOCIAL, value: -5 },
+    ],
+  },
+  {
+    title: "Watch a movie with friends?",
+    time: TIME.NIGHT,
+    effects: [
+      { pillar: PILLAR.STUDY, value: -5 },
+      { pillar: PILLAR.SOCIAL, value: 5 },
+    ],
+  },
+  {
+    title: "Take a nap?",
+    time: TIME.AFTERNOON,
+    effects: [
+      { pillar: PILLAR.STUDY, value: -5 },
+      { pillar: PILLAR.SLEEP, value: 5 },
+    ],
+  },
+  {
+    title: "Attend a club meeting?",
+    time: TIME.NIGHT,
+    effects: [
+      { pillar: PILLAR.STUDY, value: 5 },
+      { pillar: PILLAR.SOCIAL, value: 5 },
+      { pillar: PILLAR.SLEEP, value: -5 },
+    ],
+  },
+  {
+    title: "Spend the day volunteering?",
+    time: TIME.NIGHT,
+    effects: [
+      { pillar: PILLAR.STUDY, value: 10 },
+      { pillar: PILLAR.SOCIAL, value: 5 },
+      { pillar: PILLAR.SLEEP, value: -10 },
+    ],
+  },
+  {
+    title: "Go out for drinks with coworkers?",
+    time: TIME.NIGHT,
+    effects: [
+      { pillar: PILLAR.STUDY, value: -5 },
+      { pillar: PILLAR.SOCIAL, value: 5 },
+    ],
+  },
+  {
+    title: "Stay up late studying for an exam?",
+    time: TIME.NIGHT,
+    effects: [
+      { pillar: PILLAR.STUDY, value: 10 },
+      { pillar: PILLAR.SOCIAL, value: -10 },
     ],
   },
 ];
